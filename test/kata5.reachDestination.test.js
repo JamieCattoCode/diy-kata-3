c4d04e21e68b3ade6f2a5075c230368b28327981
Jamie Catto <jamietcatto@gmail.com>
2022-10-10 10:20:01 +0100

    Completed reachDestination kata

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,10 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(10, 40)).toBe("I should be there in 0.5 hours");
+    expect(reachDestination(80, 40)).toBe("I should be there in 2 hours");
+    expect(reachDestination(236, 63)).toBe("I should be there in 3.5 hours");
+    expect(reachDestination(192, 34)).toBe("I should be there in 5.5 hours");
+  });
 });
