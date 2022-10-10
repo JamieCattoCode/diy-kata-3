a24cfb6a14e1cb54eea82cc41a06129ba95fa2fc
Jamie Catto <jamietcatto@gmail.com>
2022-10-10 09:02:50 +0100

    Completed katas for fizzBuzz and booleanToWord

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,16 @@
-const fizzBuzz = (number) => {};
+const fizzBuzz = (number) => {
+    let output = "";
+
+    if (number % 3 === 0) {
+        output += "Fizz";
+    }
+    if (number % 5 === 0) {
+        output += "Buzz";
+    }
+    if (number % 3 != 0 && number % 5 != 0) {
+        output += number;
+    }
+    return output;
+};
 
 module.exports = fizzBuzz;
