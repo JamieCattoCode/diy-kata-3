d456b0b434906026edac82a76eca548c6a2b2aef
Jamie Catto <jamietcatto@gmail.com>
2022-10-10 09:19:12 +0100

    Completed numberToReversedDigits kata

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,8 @@
-const numberToReversedDigits = (number) => {};
+const numberToReversedDigits = (number) => {
+    const stringed = number.toString();
+    let arrayed = stringed.split('');
+    arrayed = arrayed.reverse();
+    return arrayed.map(n => Number(n));
+};
 
 module.exports = numberToReversedDigits;
